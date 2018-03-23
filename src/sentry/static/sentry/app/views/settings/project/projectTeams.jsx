c39ca2420c5cb2dfa26219d2828d7ad35b2c8317
chrissy <fivefourths@gmail.com>
2018-03-23 14:08:39 -0700

    first commit

--- src/sentry/static/sentry/app/views/settings/project/projectTeams.jsx
+++ src/sentry/static/sentry/app/views/settings/project/projectTeams.jsx
@@ -27,6 +27,8 @@ import PanelBody from '../components/panelBody';
 import PanelHeader from '../components/panelHeader';
 import PanelItem from '../components/panelItem';
 import SettingsPageHeader from '../components/settingsPageHeader';
+import classNames from 'classnames';
+import {verticalPaddingSmall, horizontalPaddingExtaSmall} from '../../../styles/padding';
 
 const TeamRow = createReactClass({
   displayName: 'TeamRow',
@@ -181,7 +183,11 @@ class ProjectTeams extends AsyncView {
       .map(team => ({
         value: team.id,
         searchKey: team.slug,
-        label: <TeamDropdownElement>#{team.slug}</TeamDropdownElement>,
+        label: (
+          <div className={classNames(verticalPaddingSmall, horizontalPaddingExtaSmall)}>
+            #{team.slug}
+          </div>
+        ),
       }));
 
     let menuHeader = (
@@ -220,6 +226,12 @@ class ProjectTeams extends AsyncView {
 
     return (
       <PanelBody>
+        <PanelHeader key={'header'} hasButtons={true}>
+          <PanelHeaderContent>
+            <div>{t('Team')}</div>
+            <div>{this.renderAddTeamButton()}</div>
+          </PanelHeaderContent>
+        </PanelHeader>
         {this.state.projectTeams.map(team => {
           return (
             <TeamRow
@@ -291,12 +303,11 @@ const RemoveIcon = styled(props => (
   margin-right: 0.5em;
 `;
 
-const TeamDropdownElement = styled('div')`
-  text-transform: none;
 
-  &:hover {
-    cursor: pointer;
-  }
+const PanelHeaderContent = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const StyledPanelItem = styled(PanelItem)`
